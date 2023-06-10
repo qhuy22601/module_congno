e4ec6b5277c803066d4801a6b964951f9939b02e
qhuy22601 <qhuy22601@gmail.com>
2023-06-10 14:54:42 +0700

    email

--- src/component/Statistic.js
+++ src/component/Statistic.js
@@ -1,7 +1,22 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Button, Form, Input, Modal, Space, Table } from "antd";
-import { CheckCircleTwoTone } from "@ant-design/icons";
+import {
+  CheckCircleTwoTone,
+  FilePdfOutlined,
+  DownloadOutlined,
+  FileWordOutlined,
+  PictureOutlined,
+} from "@ant-design/icons";
+
+import * as XLSX from "xlsx";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import "jspdf-autotable";
+import { saveAs } from "file-saver";
+import Docxtemplater from "docxtemplater";
+import JSZip from "jszip";
+
 
 export default function Statistic(){
 
@@ -74,6 +89,47 @@ export default function Statistic(){
       },
     ];
 
+    const tableRef = useRef();
+
+    const handleDownloadExcel = () => {
+      const flattenedData = data.map((item) => ({
+        ...item,
+        mst: item.user && item.user.mst,
+        name: item.user && item.user.name,
+        email: item.user && item.user.contactEmail,
+      }));
+
+      const workbook = XLSX.utils.book_new();
+      const worksheet = XLSX.utils.json_to_sheet(flattenedData);
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+      XLSX.writeFile(workbook, "table_data.xlsx");
+    };
+
+     const handleDownloadPDF = async () => {
+       const table = tableRef.current;
+       const canvas = await html2canvas(table, { useCORS: true });
+       const imageData = canvas.toDataURL("image/png");
+
+       const pdf = new jsPDF("p", "pt", "a4");
+       const imgProps = pdf.getImageProperties(imageData);
+       const pdfWidth = pdf.internal.pageSize.getWidth();
+       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+       pdf.addImage(imageData, "PNG", 0, 0, pdfWidth, pdfHeight);
+       pdf.save("table.pdf");
+     };
+
+
+     const handleDownloadPNG = () => {
+       html2canvas(tableRef.current).then((canvas) => {
+         const image = canvas.toDataURL("image/png");
+         const link = document.createElement("a");
+         link.href = image;
+         link.download = "table.png";
+         link.click();
+       });
+     };
+
     const handleCheckClick = async (id) => {
       try {
         await axios.post(`/api/debt/save-to-excel/${id}`);
@@ -96,7 +152,36 @@ export default function Statistic(){
   }, []);
 
     return (
-      <Table dataSource={data} columns={columns}>
-      </Table>
+      <div>
+        <div>
+          <Button
+            type="primary"
+            icon={<DownloadOutlined />}
+            onClick={handleDownloadExcel}
+          >
+            Excel
+          </Button>
+          <Button
+            type="primary"
+            icon={<FilePdfOutlined />}
+            onClick={handleDownloadPDF}
+          >
+            PDF
+          </Button>
+          <Button
+            type="primary"
+            icon={<PictureOutlined />}
+            onClick={handleDownloadPNG}
+          >
+            PNG
+          </Button>
+        </div>
+        <Table
+          dataSource={data}
+          columns={columns}
+          tableLayout="fixed"
+          ref={tableRef}
+        />
+      </div>
     );
 }
