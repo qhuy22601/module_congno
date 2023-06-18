329725020bc84dff38b9d8240c4ca22abafd78ca
qhuy22601 <qhuy22601@gmail.com>
2023-06-18 10:14:19 +0700

    UI: del search's logo

--- src/component/accountItem/index.js
+++ src/component/accountItem/index.js
@@ -1,25 +1,30 @@
 import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
 import { Link } from "react-router-dom";
-
+import Card from "antd"
 function AccountItem({ data }) {
   const cx = classNames.bind(styles);
   console.log(data);
   return (
-    <div to={`fruit-detail/${data.id}`} className={cx("info-wrapper")}>
-      <img className={cx("info-avatar")} src={`${data.logo}`} alt="logo" />
+    <Link
+      style={{ textDecoration: "none", color: "black" }}
+      to={`/user/${data.id}`}
+      className={cx("info-wrapper")}
+    >
+      {/* <img className={cx("info-avatar")} src={`${data.logo}`} alt="logo" /> */}
       <div className={cx("info-user")}>
         <h4 className={cx("mst")}>
-          <span>{`${data.mst}`}</span><br/>
+          <span> {`${data.name}`}</span>
+          <br />
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <span className={cx("name")}>{`${data.name}`}</span>
+          <span className={cx("name")}>MST:{`${data.mst}`}</span>
         </h4>
       </div>
-    </div>
+    </Link>
   );
 }
 
